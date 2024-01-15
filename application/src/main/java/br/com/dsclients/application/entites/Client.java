05e2bbd770e5b072bfc72e7974d6457c931ce835
Lucas Santos <lucas201273@gmail.com>
2024-01-15 18:10:19 -0300

    feat: add client entity

---
+++ application/src/main/java/br/com/dsclients/application/entites/Client.java
@@ -0,0 +1,43 @@
+package br.com.dsclients.application.entites;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.io.Serializable;
+import java.time.Instant;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name="tb_clients")
+public class Client implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String cpf;
+
+    private Double income;
+
+    private Instant birthDate;
+
+    private Integer children;
+
+    @CreationTimestamp
+    @Column(name="created_at")
+    private Instant createadAt;
+
+    @CreationTimestamp
+    @Column(name="updated_at")
+    private Instant updatedAt;
+}
